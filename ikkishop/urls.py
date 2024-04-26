e9b1580ea3fb161b009e29a55798396fcdaf367c
miompolly <57joel39@gmail.com>
2024-04-26 08:50:12 +0200

    display categories

--- ikkishop/urls.py
+++ ikkishop/urls.py
@@ -18,10 +18,11 @@ from ikkishop import settings
 from ikkishop.settings import MEDIA_ROOT
 from . import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='home'),
+    path('store/',include('store.urls')),
 ]+static(settings.MEDIA_URL,document_root=MEDIA_ROOT)
--- ikkishop/views.py
+++ ikkishop/views.py
@@ -2,11 +2,12 @@
 from django.shortcuts import render
 
 from store.models import Product
+from category.models import Category
 
 
 def home(request):
     products=Product.objects.all().filter(is_available=True)
-
+    
     context={
         'products': products,
     }
