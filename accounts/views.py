117a9d7f53356a4771ce77893dbab6bc9d5c3e86
miompolly <57joel39@gmail.com>
2024-07-01 10:11:34 +0200

    login with credentials

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib import messages
+from django.contrib import messages,auth
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from accounts.models import Account
@@ -29,9 +30,25 @@ def register(request):
     return render(request, 'accounts/register.html',context)
 
 def login(request):
-    # Your login logic here
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+
+ 
     return render(request, 'accounts/login.html')
 
+@login_required(login_url = 'login')
 def logout(request):
-    # Your logout logic here
-    return redirect('accounts/login')
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect('login')
